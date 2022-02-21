223dd2f606ecf182f551a940220435d7c8205b35
nvylegzhanina <n.vylegzhanina@icloud.com>
2022-02-21 18:08:37 +0300

    fix task 4

--- Calculator/src/com/company/homework4/UserMessagesWorker.java
+++ Calculator/src/com/company/homework4/UserMessagesWorker.java
@@ -3,25 +3,30 @@ package com.company.homework4;
 import java.util.Scanner;
 
 public class UserMessagesWorker {
-    private static Scanner scanner;
+    private Scanner scanner;
+
+    public UserMessagesWorker(){
+        scanner = new Scanner(System.in);
+    }
 
     public void sendMessageToUser(String message){
         System.out.println(message);
     }
 
     public double getDoubleFromUser(){
-        scanner = new Scanner(System.in);
         double number = scanner.nextDouble();
-        scanner.close();
 
         return number;
     }
 
     public String getStringFromUser(){
-        scanner = new Scanner(System.in);
         String op = scanner.next();
-        scanner.close();
-
         return op;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        scanner.close();
+        super.finalize();
     }
 }
